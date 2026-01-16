6c2eda608bcff2a539970f0354621d661a0317f6
Max Herold <maxoheyn@gmail.com>
2026-01-16 17:38:22 -0500

    wip starting point

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1163,3 +1163,9 @@ EventScript_VsSeekerChargingDone::
 	.include "data/scripts/dexnav.inc"
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
+
+	.include "data/maps/startcity/scripts.inc"
+
+	.include "data/maps/StartCity/scripts.inc"
+
+	.include "data/maps/SampleMapTest/scripts.inc"
